1e37c0f047b5aa920b48fe28317c6b931bbd3389
Moaz Reyad <moaz.reyad@univ-grenoble-alpes.fr>
2020-03-02 19:22:58 +0100

    test

--- tool/rat/rat.sh
+++ tool/rat/rat.sh
@@ -1,4 +1,6 @@
-mvn apache-rat:check
+wget https://downloads.apache.org//creadur/apache-rat-0.13/apache-rat-0.13-bin.tar.bz2
+tar -xvf ./apache-rat-0.13-bin.tar.bz2 
+java -jar apache-rat-0.13/apache-rat-0.13.jar .
 
 if [ $? != 0 ]; then
     echo "${red}Rat check failed.${reset}"
