df8bc0860e6ae566ee690ae50aa3b69c313c2de8
Moaz Reyad <moazreyad@gmail.com>
2020-07-19 12:50:09 +0200

    Update run.py

--- test/python/run.py
+++ test/python/run.py
@@ -16,11 +16,16 @@
 # limitations under the License.
 #
 
+import sys
 import unittest
 # import xmlrunner
+def main():
+    loader = unittest.TestLoader()
+    tests = loader.discover('.')
+    testRunner = unittest.runner.TextTestRunner()
+    # testRunner = xmlrunner.XMLTestRunner(output='.')
+    ret = not testRunner.run(tests).wasSuccessful()
+    sys.exit(ret)
 
-loader = unittest.TestLoader()
-tests = loader.discover('.')
-testRunner = unittest.runner.TextTestRunner()
-# testRunner = xmlrunner.XMLTestRunner(output='.')
-testRunner.run(tests)
+if __name__ == "__main__":
+    main()
